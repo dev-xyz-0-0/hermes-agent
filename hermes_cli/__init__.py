af79c92aee94f48ec0c81613f92431dbbbe4b973
dev-xyz-0-0 <bernardchoo.dev@gmail.com>
2026-05-27 18:44:33 +0800

    chore: bump version to v0.8.11 (2026.5.27)

--- hermes_cli/__init__.py
+++ hermes_cli/__init__.py
@@ -11,5 +11,5 @@ Provides subcommands for:
 - hermes cron          - Manage cron jobs
 """
 
-__version__ = "0.8.10"
-__release_date__ = "2026.5.17"
+__version__ = "0.8.11"
+__release_date__ = "2026.5.27"
